9e0dd26eb915125974e3775048a7dfe1bfb861f9
Varun M S <varunms70@gmail.com>
2024-12-26 12:41:51 +0530

    Assignment 2: Add comments and documentations for longest valid parentheses substring problem

--- src/main/java/org/example/dayTwo/LongestValidParenthesesSubstring/LongestValidParenthesesSubstring.java
+++ src/main/java/org/example/dayTwo/LongestValidParenthesesSubstring/LongestValidParenthesesSubstring.java
@@ -2,6 +2,9 @@ package org.example.dayTwo.LongestValidParenthesesSubstring;
 
 import java.util.Stack;
 
+/**
+ * To find the longest parentheses substring
+ */
 public class LongestValidParenthesesSubstring {
     public static void main(String[] args) {
         String input = ")()())";
@@ -10,6 +13,14 @@ public class LongestValidParenthesesSubstring {
         System.out.println(output);
     }
 
+    /**
+     * getLongestValidParentheses method calculates the length of the longest valid parentheses in a given character array.
+     * A valid substring is defined as one where every opening parenthesis has a corresponding closing
+     * parenthesis in the correct order.
+     *
+     * @param input in character array
+     * @return calculate length in int
+     */
     static int getLongestValidParentheses(char[] input) {
         boolean canStart;
         int validNumber = 0;
@@ -18,22 +29,33 @@ public class LongestValidParenthesesSubstring {
         int lastInvalidNumber = 0;
         Stack stack = new Stack();
 
+        // Loops over the input character array and calculate length
         for(char s : input) {
+            // If stack is empty and starts with closing parenthesis ')', then set to false else true
             canStart = !(stack.isEmpty() && s == ')');
 
+            // Will enter the condition if char is a valid condition
             if(canStart) {
                 if(s == '(') {
                     stack.push(s);
                 } else if(s == ')') {
                     stack.pop();
+                    // tempNumber variable holds current value before assigning it to validNumber
                     tempNumber = tempNumber + 2;
 
+                    // Set current stack size if the stack is not empty; otherwise, set 0
                     invalidNumber = (!stack.isEmpty()) ? stack.size() : 0;
+
+                    // Assigns tempNumber to validNumber.
+                    // Updates validNumber if tempNumber is greater and the stack is empty or contains invalid parentheses.
                     if(tempNumber > validNumber && (stack.isEmpty() || lastInvalidNumber != invalidNumber)) {
                         validNumber = tempNumber;
                         tempNumber = 0;
                     }
 
+                    // Check if the current invalid number differs from the last tracked invalid number.
+                    // If they differ, reset tempNumber to 2 and update lastInvalidNumber to the current invalidNumber.
+                    // Otherwise, increment tempNumber by 2 to account for a valid pair of parentheses.
                     if(lastInvalidNumber != invalidNumber) {
                         tempNumber = 2;
                         lastInvalidNumber = invalidNumber;
