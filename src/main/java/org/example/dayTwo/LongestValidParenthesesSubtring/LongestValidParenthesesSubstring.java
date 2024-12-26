7c1a3f101a94c071b0411896e79aa4f421955825
Varun M S <varunms70@gmail.com>
2024-12-26 10:41:15 +0530

    Assignment 2: Longest Valid Parentheses Substring

---
+++ src/main/java/org/example/dayTwo/LongestValidParenthesesSubtring/LongestValidParenthesesSubstring.java
@@ -0,0 +1,49 @@
+package org.example.dayTwo.LongestValidParenthesesSubtring;
+
+import java.util.Stack;
+
+public class LongestValidParenthesesSubstring {
+    public static void main(String[] args) {
+        String input = ")()())";
+
+        int output = getLongestValidParentheses(input.toCharArray());
+        System.out.println(output);
+    }
+
+    static int getLongestValidParentheses(char[] input) {
+        boolean canStart;
+        int validNumber = 0;
+        int tempNumber = 0;
+        int invalidNumber;
+        int lastInvalidNumber = 0;
+        Stack stack = new Stack();
+
+        for(char s : input) {
+            canStart = !(stack.isEmpty() && s == ')');
+
+            if(canStart) {
+                if(s == '(') {
+                    stack.push(s);
+                } else if(s == ')') {
+                    stack.pop();
+                    tempNumber = tempNumber + 2;
+
+                    invalidNumber = (!stack.isEmpty()) ? stack.size() : 0;
+                    if(tempNumber > validNumber && (stack.isEmpty() || lastInvalidNumber != invalidNumber)) {
+                        validNumber = tempNumber;
+                        tempNumber = 0;
+                    }
+
+                    if(lastInvalidNumber != invalidNumber) {
+                        tempNumber = 2;
+                        lastInvalidNumber = invalidNumber;
+                    } else {
+                        tempNumber = tempNumber + 2;
+                    }
+                }
+            }
+        }
+
+        return validNumber;
+    }
+}
